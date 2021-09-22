5d4817e6f5a714c3e3e1bc4cd0bf967587ed1481
Lusajo Malango <luckybyc@gmail.com>
2021-09-22 18:26:41 +0300

    custom widget for questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,18 +9,18 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-  void answerQuestion() {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
 
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -37,16 +38,16 @@ class MyAppState extends State<MyApp> {
           ),
           body: Column(
             children: [
-              Text(
-                questions[questionIndex],
+              Question(
+                questions[_questionIndex],
               ),
               ElevatedButton(
                 child: Text('Answer1'),
-                onPressed: answerQuestion,
+                onPressed: _answerQuestion,
               ),
               ElevatedButton(
                 child: Text('Answer2'),
-                onPressed: answerQuestion,
+                onPressed: _answerQuestion,
               ),
               ElevatedButton(
                 child: Text('Answer 3'),
